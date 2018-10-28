12892638971fab5cd1fa57f82d6ef7f8ba102c51
vcsawant <viren.c.sawant@gmail.com>
2018-10-27 22:16:41 -0500

    updates

--- webpack.dev.js
+++ webpack.dev.js
@@ -29,16 +29,5 @@ module.exports = {
       }
     ]
   },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendor",
-          chunks: "all"
-        }
-      }
-    }
-  },
   plugins: []
 };
---
+++ webpack.prod.js
@@ -0,0 +1,33 @@
+/* Copyright G. Hemingway, 2018 - All rights reserved */
+"use strict";
+
+let path = require("path"),
+  webpack = require("webpack");
+
+module.exports = {
+  context: path.join(__dirname, "/src/client"),
+  entry: "./main.js",
+  mode: "production",
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "public/js")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: ["css-loader"]
+      },
+      {
+        test: /\.md$/,
+        use: ["raw-loader"]
+      }
+    ]
+  },
+  plugins: []
+};
