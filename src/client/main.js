fd5502d846d261c9ef6a5582609cbcf375fd237e
vcsawant <viren.c.sawant@gmail.com>
2018-11-01 02:14:41 -0500

    single js file

--- src/client/main.js
+++ src/client/main.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 import ReactMarkdown from "react-markdown";
-import md from "./pages/mentor-guide.md";
+import hacker_md from "./pages/hacker-guide.md";
+import mentor_md from "./pages/mentor-guide.md";
 import "./css/main.css";
 
 const transformImageUri = uri => {
@@ -11,7 +12,11 @@ const transformImageUri = uri => {
 const App = props => {
   return (
     <ReactMarkdown
-      source={md}
+      source={
+        window.location.hostname === "mentor.vandyhacks.org"
+          ? mentor_md
+          : hacker_md
+      }
       escapeHtml={false}
       transformImageUri={transformImageUri}
       linkTarget={(url, text, title) =>
