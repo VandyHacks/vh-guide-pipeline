df1c4ddc210882dc51a60e2889a1af9adf40a5a8
cktang88 <arctangent20@yahoo.com>
2018-11-01 02:22:32 -0500

    Remove duplicate JS

--- src/client/hacker-guide.js
+++ src/client/hacker-guide.js
@@ -1,24 +1,5 @@
-import React from "react";
-import { render } from "react-dom";
-import ReactMarkdown from "react-markdown";
 import md from "./pages/hacker-guide.md";
-import "./css/main.css";
+import { renderApp } from "./common.js";
 
-const transformImageUri = uri => {
-  return uri;
-};
+renderApp(md);
 
-const App = props => {
-  return (
-    <ReactMarkdown
-      source={md}
-      escapeHtml={false}
-      transformImageUri={transformImageUri}
-      linkTarget={(url, text, title) =>
-        url.indexOf("#") > -1 ? undefined : "_blank"
-      }
-    />
-  );
-};
-
-render(<App />, document.getElementById("content"));
