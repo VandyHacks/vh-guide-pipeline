94d2fb6cd096a3d4686f80057dd81075fabc75bb
Matt Leon <matthew.leon@vanderbilt.edu>
2019-10-31 11:47:35 -0500

    Fix FOUC

--- src/client/common.js
+++ src/client/common.js
@@ -1,34 +1,31 @@
-/** 
- * 
+/**
+ *
  * Rendering pipeline from markdown --> React, shared by all .md file conversions.
- * 
+ *
  */
 
 import React from "react";
 import { render } from "react-dom";
 import ReactMarkdown from "react-markdown";
-import "./css/main.css";
 
 const transformImageUri = uri => {
-    return uri;
-  };
-  
-const App = props => {
-    return (
-        <ReactMarkdown
-        source={props.md}
-        escapeHtml={false}
-        transformImageUri={transformImageUri}
-        linkTarget={(url, text, title) =>
-            url.indexOf("#") > -1 ? undefined : "_blank"
-        }
-        />
-    );
+  return uri;
 };
 
-const renderApp = (src) => render(<App md={src}/>, document.getElementById("content"));
+const App = props => {
+  return (
+    <ReactMarkdown
+      source={props.md}
+      escapeHtml={false}
+      transformImageUri={transformImageUri}
+      linkTarget={(url, text, title) =>
+        url.indexOf("#") > -1 ? undefined : "_blank"
+      }
+    />
+  );
+};
 
-export {
-    renderApp
-}
-  
+const renderApp = src =>
+  render(<App md={src} />, document.getElementById("content"));
+
+export { renderApp };
